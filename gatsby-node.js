5dd78c897bd3ed2c6f7ca427df7ea8bb9b4cd7c0
Kerrie Hui <kerrie.hui@gmail.com>
2020-10-17 11:14:45 +1100

    HOT FIX: static build

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,51 +1,51 @@
-exports.createPages = async ({ graphql, actions }) => {
-  const { createPage } = actions
-  const result = await graphql(
-    `
-      {
-        projects: allStrapiProject {
-          edges {
-            node {
-              strapiId
-            }
-          }
-        }
-        categories: allStrapiCategory {
-          edges {
-            node {
-              strapiId
-            }
-          }
-        }
-      }
-    `
-  )
+// exports.createPages = async ({ graphql, actions }) => {
+//   const { createPage } = actions
+//   const result = await graphql(
+//     `
+//       {
+//         projects: allStrapiProject {
+//           edges {
+//             node {
+//               strapiId
+//             }
+//           }
+//         }
+//         categories: allStrapiCategory {
+//           edges {
+//             node {
+//               strapiId
+//             }
+//           }
+//         }
+//       }
+//     `
+//   )
 
-  if (result.errors) {
-    throw result.errors
-  }
+//   if (result.errors) {
+//     throw result.errors
+//   }
 
-  // Create blog projects pages.
-  const projects = result.data.projects.edges
-  const categories = result.data.categories.edges
+//   // Create blog projects pages.
+//   const projects = result.data.projects.edges
+//   const categories = result.data.categories.edges
 
-  projects.forEach((project, index) => {
-    createPage({
-      path: `/project/${project.node.strapiId}`,
-      component: require.resolve("./src/layouts/project.js"),
-      context: {
-        id: project.node.strapiId,
-      },
-    })
-  })
+//   projects.forEach((project, index) => {
+//     createPage({
+//       path: `/project/${project.node.strapiId}`,
+//       component: require.resolve("./src/layouts/project.js"),
+//       context: {
+//         id: project.node.strapiId,
+//       },
+//     })
+//   })
 
-  categories.forEach((category, index) => {
-    createPage({
-      path: `/category/${category.node.strapiId}`,
-      component: require.resolve("./src/layouts/category.js"),
-      context: {
-        id: category.node.strapiId,
-      },
-    })
-  })
-}
+//   categories.forEach((category, index) => {
+//     createPage({
+//       path: `/category/${category.node.strapiId}`,
+//       component: require.resolve("./src/layouts/category.js"),
+//       context: {
+//         id: category.node.strapiId,
+//       },
+//     })
+//   })
+// }
